4b8ab6fddf68bf313308e00843b063139e67eeb9
tomer <tomertal52@yahoo.com>
2022-12-13 23:20:04 -0500

    commit tree 284e237862db9b4dc5d1333ee86cc08e45ab29e3

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function passChange(event){
+        console.log(`Entering password...`);
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={passChange} type="password"></input>
+        </div>
     )
 }
 
